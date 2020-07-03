823492aea6d44b3f1f10d24cdcdf90ebd0e42a77
Guilherme Frankenber <nemseialistar@gmail.com>
2020-07-03 18:58:10 -0300

    correcao services

--- src/app/restaurant/shop/shop.service.ts
+++ src/app/restaurant/shop/shop.service.ts
@@ -1,16 +1,16 @@
 import { Shop } from "app/restaurant/shop/shop.model";
-import { Http } from "@angular/http";
 import { MEAT_API } from "app/app.api";
+import { Injectable } from "@angular/core";
 import { Observable } from "rxjs/Observable";
 
+
 import "rxjs/add/operator/map";
+import { HttpModule, Http } from "@angular/http";
 
-
+@Injectable()
 export class ShopService {
     constructor(private https: Http) {
     }
 
-    shops(): Observable<Shop[]> {
-        return this.https.get(MEAT_API+"/restaurants").map(response => response.json());
-    }
+
 }
